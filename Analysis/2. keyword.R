a25c2fccc6a9131affc969aa8118f60625f57b3b
Sooahn Shin <anne940303@gmail.com>
2019-03-08 10:15:53 +0900

    keyword timeline

--- Analysis/2. keyword.R
+++ Analysis/2. keyword.R
@@ -46,29 +46,21 @@ ggplot(monthly_n) +
 
 
 ## unigram keyword
-# news_unigrams <- news_data %>%
-#   unnest_tokens(ngram, text, token = "ngrams", n = 1) %>%
-#   filter(!ngram %in% stop_words$word) %>%
-#   mutate(stemmed = wordStem(ngram))
-# 
-# news_unigrams_by_article <- news_unigrams %>%
-#   count(id_row, ngram) %>%
-#   bind_tf_idf(ngram, id_row, n) %>%
-#   arrange(desc(tf_idf)) %>%
-#   left_join(news_data[,c("id_row","month")]) %>%
-#   group_by(month,ngram) %>%
-#   mutate(n_month = n(), tf_idf_month = sum(tf_idf)) %>%
-#   ungroup()
-# 
-# news_unigrams_by_article %>%
-#   select(ngram,month,n_month,tf_idf_month) %>%
-#   distinct() %>%
-#   group_by(month) %>%
-#   top_n(5, wt=n_month) %>%
-#   arrange(month,-n_month)
+news_unigrams <- news_data %>%
+  unnest_tokens(ngram, text, token = "ngrams", n = 1) %>%
+  filter(!ngram %in% stop_words$word) %>%
+  mutate(stemmed = wordStem(ngram))
+
+news_unigrams_by_article <- news_unigrams %>%
+  count(id_row, ngram) %>%
+  bind_tf_idf(ngram, id_row, n) %>%
+  arrange(desc(tf_idf)) %>%
+  left_join(news_data[,c("id_row","year","month", "date")]) %>%
+  group_by(year, month, ngram) %>%
+  mutate(n_month = n(), tf_idf_month = sum(tf_idf)) %>%
+  ungroup()
 
 ## bigram keyword
-
 news_bigrams <- news_data %>%
   unnest_tokens(ngram, text, token = "ngrams", n = 2) %>%
   separate(ngram, c("word1", "word2"), sep = " ") %>%
@@ -80,7 +72,7 @@ news_bigrams_by_article <- news_bigrams %>%
   count(id_row, ngram) %>%
   bind_tf_idf(ngram, id_row, n) %>%
   arrange(desc(tf_idf)) %>%
-  left_join(news_data[,c("id_row","year","month")]) %>%
+  left_join(news_data[,c("id_row","year","month", "date")]) %>%
   group_by(year, month, ngram) %>%
   mutate(n_month = n(), tf_idf_month = sum(tf_idf)) %>%
   ungroup()
@@ -130,5 +122,120 @@ p1
 dev.off()
 
 #########################
-## total frequency plot
+## timeline plot
 #########################
+
+news_unigrams_by_article <- news_unigrams_by_article %>%
+  group_by(date,ngram) %>%
+  mutate(n_date = n(), tf_idf_date = sum(tf_idf)) %>%
+  ungroup()
+
+news_bigrams_by_article <- news_bigrams_by_article %>%
+  group_by(date,ngram) %>%
+  mutate(n_date = n(), tf_idf_date = sum(tf_idf)) %>%
+  ungroup()
+
+##############################################
+###----------  Plot A : 긴장국면 ----------###
+##############################################
+a <- rbind(news_unigrams_by_article, news_bigrams_by_article) %>%
+  filter(ngram%in%c("bloody nose", "maximum pressure", "preemptive strike")) %>%
+  mutate(ngram = recode(ngram, "bloody nose" = "Bloody Nose     ",
+         "maximum pressure" = "Maximum Pressure     ",
+         "preemptive strike" = "Preemptive Strike     "))
+
+# !!!NOTE THAT!!!
+# relative frequency = tf_idf_date
+a %>%
+  select(date, tf_idf_date, ngram) %>%
+  distinct() %>%
+  ggplot(., aes(x=date, y=tf_idf_date, group=ngram, color=ngram, shape=ngram)) + 
+  geom_point(size=1, alpha=0.6) +
+  stat_smooth(geom="line", size=1, alpha=0.5, method = 'lm', formula = y ~ poly(x,2), se= FALSE) +
+  scale_x_date(date_minor_breaks = "1 day") + xlab("") + ylab("relative frequency") +
+  scale_color_manual(values = wes_palette(n=3, name="Darjeeling1")) +
+  theme_minimal() + theme(legend.title = element_blank(),legend.position="top") +
+  labs(caption = "Copyright: SNU IIS Global Data Center")
+
+
+##############################################
+###----------  Plot B : 대화국면 ----------###
+##############################################
+b <- rbind(news_unigrams_by_article, news_bigrams_by_article) %>%
+  filter(ngram%in%c("korea summit", "panmunjom", "peace treaty")) %>%
+  mutate(ngram = recode(ngram, "korea summit" = "Korea Summit     ",
+                        "panmunjom" = "Panmunjom     ",
+                        "peace treaty" = "Peace Treaty     "))
+
+b %>%
+  select(date, tf_idf_date, ngram) %>%
+  distinct() %>%
+  ggplot(., aes(x=date, y=tf_idf_date, group=ngram, color=ngram, shape=ngram)) + 
+  geom_point(size=1, alpha=0.6) +
+  stat_smooth(geom="line", size=1, alpha=0.5, method = 'lm', formula = y ~ poly(x,2), se= FALSE) +
+  scale_x_date(date_minor_breaks = "1 day") + xlab("") + ylab("relative frequency") +
+  scale_color_manual(values = wes_palette(n=3, name="Darjeeling1")) +
+  theme_minimal() + theme(legend.title = element_blank(),legend.position="top") +
+  labs(caption = "Copyright: SNU IIS Global Data Center")
+
+##############################################
+###----------    Plot C : 북핵   ----------###
+##############################################
+c <- rbind(news_unigrams_by_article, news_bigrams_by_article) %>%
+  filter(ngram%in%c("northkorea iran", "nuclear test", "nuclearweapon")) %>%
+  mutate(ngram = recode(ngram, "northkorea iran" = "North Korea Iran     ",
+                        "nuclear test" = "Nuclear Test     ",
+                        "nuclearweapon" = "Nuclear Weapon     "))
+
+c %>%
+  select(date, tf_idf_date, ngram) %>%
+  distinct() %>%
+  ggplot(., aes(x=date, y=tf_idf_date, group=ngram, color=ngram, shape=ngram)) + 
+  geom_point(size=1, alpha=0.6) +
+  stat_smooth(geom="line", size=1, alpha=0.5, method = 'lm', formula = y ~ poly(x,2), se= FALSE) +
+  scale_x_date(date_minor_breaks = "1 day") + xlab("") + ylab("relative frequency") +
+  scale_color_manual(values = wes_palette(n=3, name="Darjeeling1")) +
+  theme_minimal() + theme(legend.title = element_blank(),legend.position="top") +
+  labs(caption = "Copyright: SNU IIS Global Data Center")
+
+##############################################
+###----------  Plot D : 비핵화   ----------###
+##############################################
+d <- rbind(news_unigrams_by_article, news_bigrams_by_article) %>%
+  filter(ngram%in%c("denuclearization", "nuclear disarmament")) %>%
+  mutate(ngram = recode(ngram,
+                        "denuclearization" = "Denuclearization     ",
+                        "nuclear disarmament" = "Nuclear Disarmament     "))
+
+d %>%
+  select(date, tf_idf_date, ngram) %>%
+  distinct() %>%
+  ggplot(., aes(x=date, y=tf_idf_date, group=ngram, color=ngram, shape=ngram)) + 
+  geom_point(size=1, alpha=0.6) +
+  stat_smooth(geom="line", size=1, alpha=0.5, method = 'lm', formula = y ~ poly(x,2), se= FALSE) +
+  scale_x_date(date_minor_breaks = "1 day") + xlab("") + ylab("relative frequency") +
+  scale_color_manual(values = wes_palette(n=3, name="Darjeeling1")) +
+  theme_minimal() + theme(legend.title = element_blank(),legend.position="top") +
+  labs(caption = "Copyright: SNU IIS Global Data Center")
+
+##############################################
+###----------    Plot E : 경제   ----------###
+##############################################
+e <- rbind(news_unigrams_by_article, news_bigrams_by_article) %>%
+  filter(ngram%in%c("southkorea trade","northkorea trade", "korea trade", "tariff", "tpp")) %>%
+  mutate(ngram = recode(ngram, "southkorea trade" = "Korea Trade     ",
+                        "northkorea trade" = "Korea Trade     ",
+                        "korea trade" = "Korea Trade     ",
+                        "tariff" = "Tariff     ",
+                        "tpp" = "TPP     "))
+
+e %>%
+  select(date, tf_idf_date, ngram) %>%
+  distinct() %>%
+  ggplot(., aes(x=date, y=tf_idf_date, group=ngram, color=ngram, shape=ngram)) + 
+  geom_point(size=1, alpha=0.6) +
+  stat_smooth(geom="line", size=1, alpha=0.5, method = 'lm', formula = y ~ poly(x,2), se= FALSE) +
+  scale_x_date(date_minor_breaks = "1 day") + xlab("") + ylab("relative frequency") +
+  scale_color_manual(values = wes_palette(n=3, name="Darjeeling1")) +
+  theme_minimal() + theme(legend.title = element_blank(),legend.position="top") +
+  labs(caption = "Copyright: SNU IIS Global Data Center")
