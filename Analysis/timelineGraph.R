1d493b9a0dc1a91efc39dff2f5c3e9cecc9efe95
Sooahn Shin <anne940303@gmail.com>
2019-03-11 17:58:09 +0900

    monthly timeline graph

--- Analysis/timelineGraph.R
+++ Analysis/timelineGraph.R
@@ -1,56 +1,48 @@
-plot.data <- df.all %>% filter(str_detect(ngram, paste0(word.list, collapse = "|")))
+title = paste0("Words Related in ", input, " Articles") 
+
+word.list.count <- str_count(word.list, "\\S+")
+word.list.df <- sapply(word.list.count, function(x) ifelse(x==1,"df.unigram", "df.bigram"))
+word.color <- sapply(word.sentiment, function(x) switch(x, "neutral"="darkgrey", "negative"="#F8766D", "positive"="#00BFC4"))
+
+plot.data <- list()
+for (i in 1:length(word.list)) {
+  plot.data[[i]] <- get(word.list.df[i]) %>% filter(str_detect(ngram, word.list[i])) %>%
+    mutate(word = word.list[i], sent = word.sentiment[i])
+}
+plot.data <- do.call(rbind, plot.data)
+
+plot.data <- plot.data %>%
+  group_by(year, month, word, sent) %>%
+  summarise(freq = sum(n)) %>%
+  ungroup() %>%
+  mutate(date = paste0(year, "-" ,month,"-01"))
+y.position <- max(plot.data$freq)
 
 p <- plot.data %>%
-  select(date, n_month, ngram) %>%
-  distinct() %>%
-  ggplot(., aes(x=as.Date(date), y=n_month, color=ngram), group=ngram) + 
-  geom_point(size=2, alpha=0.9) +
-  scale_x_date(date_minor_breaks = "1 day", date_labels = "%Y-%m") +
+  ggplot(., aes(x=as.Date(date), y=freq, group=word, color=sent, shape=word)) + 
+  geom_point(size=2) +
+  geom_line(size=1, alpha=0.6) +
+  scale_color_manual(breaks = c("negative","neutral","positive"), values=c("#F8766D","darkgrey","#00BFC4")) +
+  scale_x_date(date_breaks = "1 month",date_minor_breaks = "1 day", date_labels = "%Y-%m") + 
   xlab("Month") + ylab("Absolute Frequency") +
-  theme(axis.text.x = element_text(angle = 25, vjust = 1.0, hjust = 1.0)) + 
-  theme_minimal() +
-  theme(legend.key=element_blank(), legend.key.size=unit(0.5,"point")) +
-  guides(colour=guide_legend(ncol=12)) + 
-  theme(legend.title = element_blank(),legend.text=element_text(size=rel(0.5)),
-        legend.position="bottom") +
-  labs(title=paste0("Bigram Words Related with ", paste0(word.list, collapse = ", "), " in ", input, " Articles"), subtitle = subtitle, caption = "Copyright: SNU IIS Global Data Center")
+  theme_minimal() + theme(legend.title = element_blank(),legend.position="top") +
+  theme(axis.text.x = element_text(angle = 25, vjust = 1.0, hjust = 1.0),
+        legend.text=element_text(size=rel(1.5), margin = margin(r=10))) + 
+  labs(title = title,caption = "Copyright: SNU IIS Global Data Center") + 
+  guides(color = FALSE,
+         shape = guide_legend(override.aes = list(color = word.color[order(word.list)])))+
+  geom_vline(xintercept= as.Date("2019-02-27"), linetype="dotted", colour ="blue")+
+  geom_vline(xintercept= as.Date("2018-11-06"), linetype="dotted", colour ="red")+
+  annotate("text", x = as.Date("2019-03-02"), y = y.position, angle=270, size = 3, 
+           label = "NK-US Hanoi Summit", hjust = 0)+
+  annotate("text", x = as.Date("2018-11-09"), y = y.position, angle=270, size = 3, 
+           label = "US Midterm Election", hjust = 0)
 
-pdf(file=paste0(file.name, "_", word.list[[i]], "_all.pdf"), family="sans", width = 12, height = 7)
+
+
+pdf(file=paste0(file.name, "_", paste0(word.list, collapse = "_"), ".pdf"), family="sans", width = 12, height = 7)
 print(p)
 dev.off()
-png(file=paste0(file.name, "_", word.list[[i]], "_all.png"), family="sans", width = 205, height = 205, units='mm', res = 150)
+png(file=paste0(file.name, "_", paste0(word.list, collapse = "_"), ".png"), family="sans", width = 205, height = 205, units='mm', res = 150)
 print(p)
 dev.off()
-
-
-
-# 
-# p.list = lapply(1:length(word.list), function(i) {
-#         title = paste0("Bigram Words Related with ", word.list[[i]], " in " input, " Articles") ;
-#         df.bigram %>% filter(str_detect(ngram, word.list[i]))%>%
-#             ## filter(ngram%in% word.list[[i]]) %>%
-#             select(date, n_month, ngram) %>%
-#             distinct() %>%
-#             ggplot(., aes(x=as.Date(date), y=n_month, color=ngram), group=ngram) + 
-#             geom_point(size=2, alpha=0.9) +
-#             scale_x_date(date_minor_breaks = "1 day", date_labels = "%Y-%m") +
-#             xlab("Month") + ylab("Absolute Frequency") +
-#             theme(axis.text.x = element_text(angle = 25, vjust = 1.0, hjust = 1.0)) + 
-#             theme_minimal() +
-#             theme(legend.key=element_blank(), legend.key.size=unit(0.5,"point")) +
-#             guides(colour=guide_legend(ncol=12)) + 
-#             theme(legend.title = element_blank(),legend.text=element_text(size=rel(0.5)),
-#                   legend.position="bottom") +
-#             labs(title=title, subtitle = subtitle, caption = "Copyright: SNU IIS Global Data Center")
-#     })
-#     for(i in 1:length(word.list)){
-#         pdf(file=paste0(file.name, "_", word.list[[i]], "_bigram.pdf"), family="sans",
-#             width = 12, height = 7)
-#         print(p.list[[i]])
-#         dev.off()
-#         png(file=paste0(file.name, "_", word.list[[i]], "_bigram.png"), family="sans",
-#             width = 205, height = 205, units='mm', res = 150)
-#         print(p.list[[i]])
-#         dev.off()
-#     }
-# }
